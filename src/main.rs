5f4ad59bb709250917f034f87823f0cc822a8990
Audun Hammer Hovda <audun@unicornis.no>
2023-05-10 11:34:10 +0200

    feat: added tests

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,20 @@ fn reverse(string: String) -> String {
     let reverse = string.chars().rev().collect();
     return reverse;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::reverse;
+
+    #[test]
+    fn ascii() {
+        let case = "hello world".to_owned();
+        assert_eq!(reverse(case), "dlrow olleh");
+    }
+
+    #[test]
+    fn utf() {
+        let case = "piña".to_owned();
+        assert_eq!(reverse(case), "añip");
+    }
+}
